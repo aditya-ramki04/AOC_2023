0ff0f5012b8107617f4899110d9df667919a6783
Aditya Ramakrishnan <112342054+aditya-ramki04@users.noreply.github.com>
2023-12-19 20:23:10 -0600

    day one

---
+++ day_one/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+      string ex = "nine1two";
+      var pattern = @"(?=(one|two|three|four|five|six|seven|eight|nine|\d))";
+
+      var transform = new Dictionary<string, string>
+        {
+            { "one", "1" },
+            { "two", "2" },
+            { "three", "3" },
+            { "four", "4" },
+            { "five", "5" },
+            { "six", "6" },
+            { "seven", "7" },
+            { "eight", "8" },
+            { "nine", "9" },
+            { "1", "1" },
+            { "2", "2" },
+            { "3", "3" },
+            { "4", "4" },
+            { "5", "5" },
+            { "6", "6" },
+            { "7", "7" },
+            { "8", "8" },
+            { "9", "9" }
+        };
+
+      var matches = Regex.Matches(ex, pattern);
+      Console.WriteLine(transform[matches.First().Groups[1].Value]);
+    }
+}
